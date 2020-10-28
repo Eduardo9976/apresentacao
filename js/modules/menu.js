794d08817d45b03084c79b30580edef1ec0e2e31
Eduardo9976 <60363926+Eduardo9976@users.noreply.github.com>
2020-10-28 17:45:42 -0300

    adicionando JS inicial

---
+++ js/modules/menu.js
@@ -0,0 +1,28 @@
+export default class Menu {
+    constructor(iconBar) {
+      this.iconBar = document.querySelector(iconBar);
+      this.active = "active";
+      this.barActive = "barActive";
+
+      this.handleClickBar = this.handleClickBar.bind(this);
+      this.handleClickList = this.handleClickList.bind(this);
+    }
+
+    handleClickBar() {
+      this.iconBar.classList.toggle(this.barActive);
+      const list = this.iconBar.nextElementSibling;
+      list.classList.toggle(this.active);
+      this.handleClickList(list);
+    }
+    handleClickList(list) {
+      list.addEventListener('click', () => {
+        this.iconBar.classList.remove(this.barActive);
+        list.classList.remove(this.active);
+      });
+    }
+    init() {
+      if(this.iconBar) {
+        this.iconBar.addEventListener('click', this.handleClickBar)
+      }
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,3 @@
+import Menu from "./modules/menu.js";
+
+const menu = new Menu(".fa-bars").init();
