3404dc403adbdd9fd4cd2c4db19cbcc14093bbad
Eduardo Fernandes <eduardo_2407@me.com>
2020-10-28 22:09:44 -0300

    Ajustando formatacao

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ import visibility from "./modules/visibility.js";
 import Visibility from "./modules/visibility.js";
 
 const menu = new Menu(".fa-bars").init();
-const tecnologiasAimate = new Visibility("#tecnologias", ".imgsTecnologias").init();
+const tecnologiasAimate = new Visibility(
+  "#tecnologias",
+  ".imgsTecnologias"
+).init();
 const contatosAnimate = new Visibility("#contatos").init();
