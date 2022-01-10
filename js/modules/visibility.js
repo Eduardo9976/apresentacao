d9a84aa5ff15a249c8f7bb7e165adbd02ab0042d
Eduardo Fernandes <eduardo_2407@me.com>
2022-01-10 11:20:03 -0300

    ajustando ft

--- js/modules/visibility.js
+++ js/modules/visibility.js
@@ -1,28 +1,28 @@
 export default class Visibility {
-  constructor(element, secondElement) {
-    this.element = document.querySelector(element);
-    this.children = [...this.element.children];
-    this.secondElement = document.querySelector(secondElement);
+  constructor (element, secondElement) {
+    this.element = document.querySelector(element)
+    this.children = [...this.element.children]
+    this.secondElement = document.querySelector(secondElement)
   }
-  init() {
+  init () {
     if (this.element) {
-      window.addEventListener("scroll", () => {
-        let mid = window.innerHeight * 0.5;
+      window.addEventListener('scroll', () => {
+        let mid = window.innerHeight * 0.5
         if (this.element.getBoundingClientRect().y < mid) {
-          this.children.forEach((child) => {
-            child.style.visibility = "visible";
-            child.classList.add("animate__animated", "animate__backInLeft");
+          this.children.forEach(child => {
+            child.style.visibility = 'visible'
+            child.classList.add('animate__animated', 'animate__backInLeft')
             // child.classList.add("animate__bounceInDown")
-          });
+          })
           if (this.secondElement) {
-            const childrenSecondElement = [...this.secondElement.children];
-            childrenSecondElement.forEach((children) => {
-              children.style.visibility = "visible";
-              children.classList.add("animate__bounceInDown");
-            });
+            const childrenSecondElement = [...this.secondElement.children]
+            childrenSecondElement.forEach(children => {
+              children.style.visibility = 'visible'
+              children.classList.add('animate__backInLeft')
+            })
           }
         }
-      });
+      })
     }
   }
 }
