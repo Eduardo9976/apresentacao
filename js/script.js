c456d7da5c444d8cfa335d85fd477e7367a74d66
Eduardo Fernandes <eduardo_2407@me.com>
2022-01-11 16:40:43 -0300

    feat: time line

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-import Menu from "./modules/menu.js";
-import Visibility from "./modules/visibility.js";
+import Menu from './modules/menu.js'
+import Visibility from './modules/visibility.js'
 
-const menu = new Menu(".fa-bars").init();
-const servicosAnimate = new Visibility("#servicos").init();
+const menu = new Menu('.fa-bars').init()
+const servicosAnimate = new Visibility('#servicos').init()
 const tecnologiasAimate = new Visibility(
-  "#tecnologias",
-  ".imgsTecnologias"
-).init();
-const contatosAnimate = new Visibility("#contatos").init();
+  '#tecnologias',
+  '.imgsTecnologias'
+).init()
+const timeLine = new Visibility('#timeLine').init()
+const contatosAnimate = new Visibility('#contatos').init()
